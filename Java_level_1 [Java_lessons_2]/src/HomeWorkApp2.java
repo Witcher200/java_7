55619631f56607af503a74dd2c864f4364990635
Igore <igore.h8k@gmail.com>
2022-08-28 09:26:17 +0300

    Урок 2

--- Java_level_1 [Java_lessons_2]/src/HomeWorkApp2.java
+++ Java_level_1 [Java_lessons_2]/src/HomeWorkApp2.java
@@ -1,11 +1,19 @@
 public class HomeWorkApp2 {
     public static void main(String[] args) {
+        System.out.println(1);
         System.out.println(m1(1, 2));
         System.out.println(m1(11, 18));
         System.out.println(m1(5, 5));
-        System.out.println(m2(5));
+        System.out.println(2);
+        m2(5);
+        m2(-4);
+        m2(0);
+        System.out.println(3);
         System.out.println(m3(4));
-        System.out.println(m4(5));
+        System.out.println(m3(-4));
+        System.out.println(4);
+        m4("Я очень люблю арбузы!!!",5);
+        m4("Finish",1);
     }
 
     public static boolean m1(int a, int b) {
@@ -16,27 +24,26 @@ public class HomeWorkApp2 {
         }
     }
 
-    public static boolean m2(int a) {
-        if (a > 0) {
-            return true;
+    public static  void m2(int a) {
+        if (a >= 0) {
+        System.out.println("Положительное");
         } else if (a < 0) {
+            System.out.println("Отрицательное");
         }
-        return false;
     }
 
     public static boolean m3(int a) {
         if (a < 0) {
-            return true;
+        return true;
         } else if (a > 0) {
         }
         return false;
     }
 
-    public static boolean m4(int a) {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Я очень люблю арбузы!!!");
+    public static void  m4(String s, int a) {
+        for (int i = 0; i < a; i++) {
+            System.out.println(s);
         }
-        return false;
     }
 }
 
